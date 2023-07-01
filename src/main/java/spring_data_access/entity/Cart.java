43d27d75af6c47972fcfd02f13b104adad694a6f
s1ngle21 <st1mxxd@gmail.com>
2023-07-01 19:53:58 +0300

    Add task

--- src/main/java/spring_data_access/entity/Cart.java
+++ src/main/java/spring_data_access/entity/Cart.java
@@ -1,12 +1,13 @@
 package spring_data_access.entity;
 
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 import java.util.*;
 
 @Component
-@Scope("prototype")
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class Cart {
 
     private Long id;
