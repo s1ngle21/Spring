af3ba77b071753a9fd8490c88c275888674efa60
s1ngle21 <st1mxxd@gmail.com>
2023-07-15 20:41:19 +0300

    Add task

--- src/main/java/app/entity/Order.java
+++ src/main/java/app/entity/Order.java
@@ -10,7 +10,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
-@Component
 @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class Order {
     private Long id;
